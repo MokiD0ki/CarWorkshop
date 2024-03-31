63c8f18bab825a1316547607b8d45c9aab1a0ed1
Yermukhamed Islam <Islam_Yermukhamed@proton.me>
2024-04-01 02:31:53 +0500

    Main window, Setting class add

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("ok")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+from settings import Settings
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.settings = Settings()
+        
+        self.setWindowTitle(self.settings.main_window_label)
+        self.setGeometry(*self.settings.main_window_geometry)
+        self.initUI()
+
+
+    def initUI(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())        
---
+++ settings.py
@@ -0,0 +1,8 @@
+import PyQt5
+
+
+class Settings:
+    def __init__(self):
+        self.main_window_geometry = (200, 200, 800, 600)
+        self.main_window_label = "Car Workshop"
+        print("Settings loaded")
